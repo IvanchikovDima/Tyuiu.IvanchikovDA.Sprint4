54d165e10c057d6f3376852ed95b590fdc9fc5a9
Tyuiu.IvanchikovDA@outlook.com <ivanchikov2022@internet.ru>
2023-11-05 15:19:48 +0500

    Add project files.

---
+++ Tyuiu.IvanchikovDA.Sprint4.Task0.V20.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.IvanchikovDA.Sprint4.Task0.V20.Lib;
+
+namespace Tyuiu.IvanchikovDA.Sprint4.Task0.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultEvenArrEl()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 4, 8, 7, 6, 5, 8, 2, 4, 3, 2 };
+            int res = ds.GetMultEvenArrEl(numsArray);
+            int wait = 24576;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.IvanchikovDA.Sprint4.Task0.V20/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IvanchikovDA.Sprint4.Task0.V20.Lib;
+
+namespace Tyuiu.IvanchikovDA.Sprint4.Task0.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт#4 |Выполнил: Иванчиков Д.А. |ПКТб-23-1                                ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#4                                                                *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Иванчиков Дмитрий Александрович | ПКТб-23-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать произведение   *");
+            Console.WriteLine("* четных элементов массива. {4 ,8 ,7 ,6 ,5 ,8 ,2 ,4 ,3 ,2}                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] numsArray = { 4, 8, 7, 6, 5, 8, 2, 4, 3, 2 };
+
+            Console.WriteLine("* Исходный массив *");
+            for (int i = 0; i < numsArray.Length - 1; i++)
+            {
+                Console.WriteLine(numsArray[i]);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" Произведение четных элементов массива =" + ds.GetMultEvenArrEl(numsArray));
+
+            Console.WriteLine();
+            Console.ReadKey();
+           
+        }
+    }
+}
